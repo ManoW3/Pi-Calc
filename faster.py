ec62f0841546cd91619b0f3db78643e5e75dedf7
Emanuel Wertman <manoisnotcool@gmail.com>
2024-12-18 08:49:00 -0500

    Better timing system, user can decide to do manual or using a library

--- faster.py
+++ faster.py
@@ -1,4 +1,5 @@
 import math
+import timeit
 import time
 
 def calcDistance(ax, ay, bx, by):
@@ -7,13 +8,12 @@ def calcDistance(ax, ay, bx, by):
 def calcMidpoint(ax, ay, bx, by):
     return [(ax+bx)*0.5, (ay+by)*0.5]
 
-def main():
+def calcPi(runNumber="Test"):
+    #print(runNumber)
     bx, by = 1, 0
     totArea = 0
-    startTime = time.time()
     for i in range(1, 30):
         if i > 2:
-            print("Run", i)
             midpoint = calcMidpoint(0, 1, bx, by)
             midpointX, midpointY = midpoint[0], midpoint[1]
             midDistance = calcDistance(0, 0, midpointX, midpointY)
@@ -24,12 +24,9 @@ def main():
             triSize = (triHeight*triWidth)*0.5
             totArea += triSize*(2**(i-2))
         elif i == 1:
-            print("Run", i)
             midpoint = [0.5, 0.5]
             midpointX, midpointY = midpoint[0], midpoint[1]
             midDistance = calcDistance(0, 0, midpointX, midpointY)
-            print(f"Midpoint: [{midpointX}, {midpointY}]")
-            print("Mid Distance:", midDistance)
             normalizedX = 1
             normalizedY = 0
             triHeight = midDistance
@@ -37,12 +34,9 @@ def main():
             triSize = (triHeight*triWidth)*0.5
             totArea += triSize
         else:
-            print("Run", i)
             midpoint = [0.5, 0.5]
             midpointX, midpointY = midpoint[0], midpoint[1]
             midDistance = calcDistance(0, 0, midpointX, midpointY)
-            print(f"Midpoint: [{midpointX}, {midpointY}]")
-            print("Mid Distance:", midDistance)
             normalizedX = midpointX/midDistance
             normalizedY = midpointY/midDistance
             triHeight = 1-midDistance
@@ -50,14 +44,32 @@ def main():
             triSize = (triHeight*triWidth)*0.5
             totArea += triSize
         
-        print("Triangle Height:", triHeight)
-        print("Triangle Width:", triWidth)
-        print("Triangle Size:", triSize, "\n")
         bx, by = normalizedX, normalizedY
-    endTime = time.time()
-    print(totArea*4)
-    print("Total Runtime:", endTime-startTime)
+    return totArea*4
 
-        
+def library():
+    runNumber = int(input("How many runs would you like to have? "))
+    execution_time = timeit.timeit("calcPi()", globals=globals(), number=runNumber)
+    print(f"Pi = {calcPi()}")
+    print(f"Total Time Taken: {execution_time} Seconds")
+    print(f"Average execution time over {runNumber} runs: {(execution_time/runNumber)*1000000:.5f} Microseconds")
+
+def manual():
+    runNumber = int(input("How many runs would you like to have? "))
+    startT = time.time()
+    for i in range(runNumber):
+        calcPi(i)
+    endT = time.time()
+    print(f"Pi = {calcPi()}")
+    print(f"Total Time Taken: {(endT-startT)} Seconds")
+    # print(f"Acerage Time Taken: {((endT-startT)/runNumber)*1000000} Microseconds")
+    print(f"Average execution time over {runNumber} runs: {((endT-startT)/runNumber)*1000000:.5f} Microseconds")
+    
+def main():
+    method = int(input("How would you like to measure time (1/2)\n    1. Library (timeit)\n    2. Manual\n"))
+    if method == 1:
+        library()
+    else:
+        manual()
 
 main()
